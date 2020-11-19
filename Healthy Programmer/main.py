21c6e73374abb7f30f74d6e77ceedc2dea275861
RF Fahad <68589534+RF-Fahad-Islam@users.noreply.github.com>
2020-11-19 20:23:47 +0600

    Add files via upload

--- Healthy Programmer/main.py
+++ Healthy Programmer/main.py
@@ -8,7 +8,7 @@ def mainsongloop(musicpath, stopper):
     mixer.music.load(musicpath)
     mixer.music.play(10)
     while True:
-        print(f"Enter \"{stopper}\"")
+        print(f"Enter \"{stopper}\" to stop. Don't forget to do the task.")
         a = input("Enter the stopper code : ")
         if a == stopper:
             mixer.music.stop()
@@ -28,9 +28,10 @@ if __name__ == "__main__":
     eyesTime = 40
     exerciseTime = 100
     while True:
-        if time() - initial_water > waterTime:
+        if time() - initial_water > waterTime and water_level != 3500:
             mainsongloop("water.wav", "drank")
             log_file("Drink Water")
+            water_level += 250
             initial_water = time()
             
         if time() - initial_eyes > eyesTime:
@@ -42,3 +43,7 @@ if __name__ == "__main__":
             mainsongloop("exercise.wav", "phydone")
             log_file("Physical exercise")
             initial_exercise = time()
+        
+        if water_level == 3500:
+            print("Your today's goal is completed of drinking 3.5 liters of water. Continue to 3.5 liters to drink water everyday to be healthy")
+            log_file("Total Drink 3.5 liters")
